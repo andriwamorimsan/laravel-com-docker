549c3b2516ea822c2210d48736ad6f6f8de08233
ANDRIW AMORIM SANTAREM <70485336+andriwamorimsan@users.noreply.github.com>
2025-03-10 20:12:29 -0300

    nova tela

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel com docker</title>
+</head>
+<body>
+
+<section class="container">
+    <div class="div">
+        <div class="card">
+            <div class="card-header rounded-5 bg-warning ">
+                    Card com nome
+            </div>
+            <div class="card-body">
+                <ul>
+                    <li class="h4">aqui fica alguma coisa </li>
+                    <li class="h5">aqui fica alguma outra coisa</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</body>
+</html>
